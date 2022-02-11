10436a6166d3116fc1264d1d7c39880f09f97d44
Metin Elitas <metinelitas@Metins-MacBook-Air.local>
2022-02-12 00:47:54 +0300

    change main.cc

--- src/main.cc
+++ src/main.cc
@@ -3,20 +3,20 @@
 
 using index_t = std::pair<int,int>;
 
-using param_t = std::map <index_t, double>;
+using composite_t = std::map <index_t, double>;
 
 class node
 {
 public:
     std::string op;
-    param_t data;
+    composite_t data;
 
     node *left = NULL, *right = NULL;
     node(std::string s)
     {
         op = s;
     }
-    node(param_t&& data)
+    node(composite_t&& data)
     {
         this->data = data;
     }
@@ -25,98 +25,83 @@ public:
 
 struct SparseExecutionEngine {
 
-
-
-
-
-    // Return the next tuple greater or equal to a given tuple
-    param_t::iterator Search(index_t index, param_t )
+    composite_t sparse_sum(composite_t lhs , composite_t rhs)
     {
+        composite_t result;
 
-    }
-
-
-   
-
-
-
-
-
-    param_t sparse_sum(param_t left , param_t right)
-    {
-        double result = 0;
-
-        // start from the lowest index of left - right 
-        index_t start = min(left.begin()->first, right.begin()->first);
-
-
-        param_t::iterator it_left = left.begin();
-        param_t::iterator it_right = right.begin();
-
-        index_t left_index = it_left->first;
-        index_t right_index = it_right->first;
-
-        if (left_index == right_index)
+        for (auto l : lhs)
         {
-            result = it_left->second + it_right->second;
+            result[l.first] = l.second;
         }
 
-
-    }
-
-    param_t sparse_multiply(param_t left , param_t right)
-    {
-
-    }
-
-    // param_t eval(node *root)
-    // {
-    //     if (!root)
-    //         return nullptr;
-
-    //     if (!root->left && !root->right)
-    //         return root->data.at(current_index);
-
+        for (auto r : rhs)
+        {
+            result[r.first] += r.second;
+        }
     
-    //     param_t l_val = eval(root->left);
-    //     param_t r_val = eval(root->right);
+        return result;
+    }
 
-    //     if (root->op == "+")
-    //     {
-    //         return sparse_sum(l_val, r_val);       
-    //     }
+    composite_t sparse_multiply(composite_t lhs , composite_t rhs)
+    {
+        composite_t result;
+        // we need matching indexes
+        for (auto l : lhs)
+        {
+            auto r = rhs.find(l.first);
+            if (r != rhs.end())
+            {
+                result.insert({l.first, l.second * r->second});
+            }
+        }
 
-    //     if (root->op == "*")
-    //     {
-    //         return sparse_multiply(l_val, r_val);
-    //     }
+        return result;
 
-    //     return sparse_sum(l_val, r_val); // default
-    // }
+    }
+
+    composite_t eval(node *root)
+    {
+        composite_t result;
+
+        if (!root)
+            return result;
+
+        if (!root->left && !root->right)
+            return root->data;
+
+        composite_t l_val = eval(root->left);
+        composite_t r_val = eval(root->right);
+
+        if (root->op == "+")
+        {
+            return sparse_sum(l_val, r_val);       
+        }
+
+        if (root->op == "*")
+        {
+            return sparse_multiply(l_val, r_val);
+        }
+
+        return sparse_sum(l_val, r_val); // default
+    }
 
 };
 
 
 
-param_t::iterator foo(param_t p)
-{
-    return p.begin();
-}
-
 // driver function to check the above program
 int main()
 {
     
-    param_t b;
-    param_t c;
-    param_t d;
-
-
-    b.insert({std::make_pair(15,15) , 8});
+    composite_t b;
+    composite_t c;
+    composite_t d;
+    
     b.insert({std::make_pair(1,2) , 3});
     b.insert({std::make_pair(2,5) , 7});
+    b.insert({std::make_pair(15,15) , 8});
 
-    c.insert({std::make_pair(1,2) , 10});
+    // c.insert({std::make_pair(1,2) , 10});
     c.insert({std::make_pair(1,3) , 10});
     c.insert({std::make_pair(1,10) , 7});
     c.insert({std::make_pair(15,15) , 9});
@@ -127,50 +112,41 @@ int main()
     d.insert({std::make_pair(15,15) , 1});
     d.insert({std::make_pair(20,20) , 2});
 
-    SparseExecutionEngine engine = SparseExecutionEngine();
 
-    // param_t a = engine.sparse_sum(b,c);
+    {
+        std::cout << "b \n -------" << std::endl;
+        for (auto val : b)
+        { 
+            std::cout << "index: [" << val.first.first << "] [" << val.first.second << "] Val = " << val.second << std::endl;
+        }
 
-    param_t::iterator it = foo(b);
+        std::cout << "c \n -------" << std::endl;
+        for (auto val : c)
+        { 
+            std::cout << "index: [" << val.first.first << "] [" << val.first.second << "] Val = " << val.second << std::endl;
+        }
 
-    std::cout << it->second << std::endl;
+        std::cout << "d \n -------" << std::endl;
+        for (auto val : d)
+        { 
+            std::cout << "index: [" << val.first.first << "] [" << val.first.second << "] Val = " << val.second << std::endl;
+        }
 
+    }
 
-
-    // {
-    //     std::cout << "b \n -------" << std::endl;
-    //     for (auto val : b)
-    //     { 
-    //         std::cout << "index: [" << val.first.first << "] [" << val.first.second << "] Val = " << val.second << std::endl;
-    //     }
-
-    //     std::cout << "c \n -------" << std::endl;
-    //     for (auto val : c)
-    //     { 
-    //         std::cout << "index: [" << val.first.first << "] [" << val.first.second << "] Val = " << val.second << std::endl;
-    //     }
-
-    //     std::cout << "d \n -------" << std::endl;
-    //     for (auto val : d)
-    //     { 
-    //         std::cout << "index: [" << val.first.first << "] [" << val.first.second << "] Val = " << val.second << std::endl;
-    //     }
-
-    // }
-
+        SparseExecutionEngine engine = SparseExecutionEngine();
 
 
         // create an expression tree
-        // node *root = new node("+");
-        // root->left = new node("*");
-        // root->left->left = new node(std::move(c));
-        // root->left->right = new node(std::move(d));
-        // root->right = new node(std::move(b));
-        // std::cout << eval(root) << std::endl;
+        node *root = new node("+");
+        root->left = new node("*");
+        root->left->left = new node(std::move(c));
+        root->left->right = new node(std::move(d));
+        root->right = new node(std::move(b));
+        
+        composite_t result = engine.eval(root);
 
-        // delete (root);
-
-    
+        delete (root);
 
 
     return 0;
